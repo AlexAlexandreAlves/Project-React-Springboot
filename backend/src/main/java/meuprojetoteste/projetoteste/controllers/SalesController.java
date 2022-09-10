a8104121cb5d6b58e8682178f500795c9f790e71
Alex <alexalexandrealves@gmail.com>
2022-09-09 23:13:31 -0300

    Date Select

--- backend/src/main/java/meuprojetoteste/projetoteste/controllers/SalesController.java
+++ backend/src/main/java/meuprojetoteste/projetoteste/controllers/SalesController.java
@@ -1,10 +1,10 @@
 package meuprojetoteste.projetoteste.controllers;
-
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import meuprojetoteste.projetoteste.entities.Sales;
@@ -18,8 +18,11 @@ public class SalesController {
 	private SalesService service;
 	
 	@GetMapping
-	public List<Sales> findSales(){
-		return service.findSales();
+	public Page<Sales> findSales(
+            @RequestParam(value="minDate", defaultValue = "") String minDate, 
+            @RequestParam(value="maxDate", defaultValue = "") String maxDate,
+            Pageable pageable) {
+        return service.findSales(minDate, maxDate,pageable);
 	}
-	
+		
 }
